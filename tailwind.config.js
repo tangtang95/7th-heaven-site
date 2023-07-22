fec660205c6126a179ac798b688f81badd66bfc0
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2023-07-22 12:22:17 +0200

    chore: move color theme comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
+            /* color-theme: https://realtimecolors.com/?colors=f8fbfb-090f10-364463-100a09-4392A4 */
             colors: {
                 white: '#F8FBFB',
                 black: '#090F10',
