b71abd88059cdc9752833a8517cb39acc21f1f5e
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-01 18:52:34 +0100

    WIP

--- src/components/mods.svelte
+++ src/components/mods.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import Spinner from './spinner.svelte';
     import { getModsFromCatalogTagRelease } from '$lib/catalog-service';
+    import { CATALOG_ID_TO_SHOW } from '$lib/constants';
 
-    const mods = getModsFromCatalogTagRelease('canary');
+    const mods = getModsFromCatalogTagRelease('canary')
+        .then(mods => mods.filter(mod => CATALOG_ID_TO_SHOW.includes(mod.ID)));
+    $: console.log(mods);
 </script>
 
 <h2 class="text-center">Mods</h2>
@@ -11,9 +14,19 @@
         <Spinner />
     </div>
 {:then mods}
-    <ul class="list-inside list-disc">
+    <ul class="list-disc list-outside">
         {#each mods as mod}
-            <li>{mod.Name}</li>
+            <li>
+                {mod.Name}<br>
+                {#if Array.isArray(mod.LatestVersion.Link)}
+                    {#each mod.LatestVersion.Link as link, i}
+                        <a href={link}>[Download with 7th]</a><br>
+                    {/each}
+                {:else}
+                    <a href={mod.LatestVersion.Link}>[Download with 7th]</a> or manual download
+                {/if}
+                
+            </li>
         {/each}
     </ul>
 {:catch}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,9 +6,16 @@ type Repository = {
 export const MOD_MANAGER_REPO: Repository = {
     owner: 'tsunamods-codes',
     repo: '7th-Heaven'
-};
+} as const;
 
 export const CATALOG_REPO: Repository = {
     owner: 'tsunamods-codes',
     repo: '7th-Heaven-Catalogs'
-};
+} as const;
+
+export const CATALOG_ID_TO_SHOW = [
+    'c0d1e191-a251-465d-8a3c-a3be5366d849', // 60/30 FPS Gameplay
+    'c33191b1-439d-4536-9c70-cc5f6957dfa2', // Avalanche Arisen Battle scenes
+    '09E81530-3F09-46B9-831B-DF431B8F319C', // Echo-S
+    '2e4c6623-97b9-4d5c-b2f3-b4f24f06a911', // Cosmo Memory
+];
